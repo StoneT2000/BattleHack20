a4633be749abc84a40d62e95aae3f45b428ad410
StoneT2000 <stoneztao@gmail.com>
2020-04-22 21:55:21 -0700

    viewer update

--- viewer/index.js
+++ viewer/index.js
@@ -1,6 +1,5 @@
 #!/usr/bin/nodejs
 
-
 // -------------- load packages -------------- //
 var express = require('express')
 var path = require('path');
@@ -9,49 +8,22 @@ var fs = require('fs');
 
 var app = express();
 
-
 // -------------- express initialization -------------- //
 app.set('port', process.env.PORT || 8080 );
 
 // tell express that the view engine is hbs
 app.set('view engine', 'hbs');
 
-
 // -------------- express endpoint definition -------------- //
 
 app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use("/node_modules", express.static(__dirname + "/node_modules"));
 
 app.get('/', function(req, res){
     console.log("home anon()");
     res.render("home", {
-        "session": req.session,
     });
 });
-
-app.get('/viewer', function(req, res){
-    console.log("viewer anon()");
-
-    res.render("viewer", {
-
-    });
-});
-
-app.get('/', function(req, res){
-    console.log("pathing_visualizer anon()");
-
-    res.render("home", {
-    });
-
-});
-
-// WILDCARD HANDLERS MUST COME AFTER ALL OTHER EXPLICIT ENDPOINTS
-app.get('/:stuff', function(req, res){
-    res.render("notfound", {
-        "session": req.session,
-        "url": req.params.stuff
-    });
-});
-
 // -------------- listener -------------- //
 // The listener is what keeps node 'alive.'
 
